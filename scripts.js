6088b3837da264a6d66bebdb721a7c75cc07d541
FrozenNotes <118176151+FrozenNotes@users.noreply.github.com>
2022-12-03 21:43:37 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
